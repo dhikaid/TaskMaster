41d1dd78ccdd623d83ebaa72dad0e31875d4ea45
Rafliramdhani16 <rafliramdhani773@gmail.com>
2024-04-28 14:23:11 +0700

    konfigurasi reactjs

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.jsx",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
