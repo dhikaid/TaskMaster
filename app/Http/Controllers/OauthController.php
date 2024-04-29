66346f0062ef31d626110164f388b1536ff08c01
DhikaID <bhadrika.223040018@mail.unpas.ac.id>
2024-04-29 18:17:19 +0700

    test fitur backend token

---
+++ app/Http/Controllers/OauthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OauthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        return dd($request);
+    }
+}
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,9 +1,25 @@
 import React from "react";
-export default function Home(data) {
+import { router } from "@inertiajs/react";
+
+export default function Home(props) {
+    const handleSubmit = () => {
+        // Form data to be submitted
+        const formData = {
+            name: "John",
+            email: "john@example.com",
+        };
+        console.log(formData);
+        // Send POST request to '/users' route with form data
+        router.post("/oauth/login", formData);
+    };
+
     return (
         <div>
             <h1 className="text-3xl font-bold underline">Welcome</h1>
-            <p className="text-xl bg-red-600 flex justify-center items-center">Hello {data.data}, welcome to your first Inertia app!</p>
+            <p className="text-xl bg-red-600 flex justify-center items-center">
+                Hello {props.csrf_token}, welcome to your first Inertia app!
+            </p>
+            <button onClick={handleSubmit}>Submit</button>
         </div>
     );
 }
