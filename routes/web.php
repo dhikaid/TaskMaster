954b3239c92ba369049085813dc41cc140998d12
DhikaID <bhadrika.223040018@mail.unpas.ac.id>
2024-04-29 17:33:37 +0700

    check routes

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
+
+// HOME
+Route::get('/a', function () {
     return Inertia::render('Home', [
         'data' => 1,
     ]);
+});
+
+// LOGIN
+Route::get('/logina', function () {
+    return Inertia::render('Home');
 });
 
 Route::get('/home', function () {
