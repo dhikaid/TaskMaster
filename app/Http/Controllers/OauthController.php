160c72d6fd023dd6dfa382ab8c6c974c76cd9bd8
DhikaID <bhadrika.223040018@mail.unpas.ac.id>
2024-04-30 16:09:55 +0700

    menambahkan logic register

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class OauthController extends Controller
 {
 
+
+    // logij
     public function login(Request $request)
     {
         return dd($request);
+    }
+
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'username' => "required|max:50|exists:users,username",
+            'email' => "email:rfc,dns|required",
+            'password1' => "required|min:8",
+            'password2' => "required|min:8|same:password1",
+        ]);
+
+        $user = User::create($validatedData);
+        return Inertia::render("Home");
     }
 }
