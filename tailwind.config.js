fec076370d4129c252c6cfe16760d6e815d08c33
Rafliramdhani16 <rafliramdhani773@gmail.com>
2024-05-01 22:29:23 +0700

    mode mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
